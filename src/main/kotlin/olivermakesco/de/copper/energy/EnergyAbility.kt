aebce53aec7e6807de51d885e29cce971952e909
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-01-13 15:43:52 -0600

    Continue with item abilities (again)

--- src/main/kotlin/olivermakesco/de/copper/energy/EnergyAbility.kt
+++ src/main/kotlin/olivermakesco/de/copper/energy/EnergyAbility.kt
@@ -5,12 +5,24 @@ import net.minecraft.entity.LivingEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.item.ItemUsageContext
 import net.minecraft.util.ActionResult
+import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
+var energyAbilities = HashMap<Identifier,EnergyAbility>()
+
 interface EnergyAbility {
+    var cooldown: Int
     fun onBlockUse(context: ItemUsageContext): ActionResult
     fun onBlockBreak(stack: ItemStack, world: World, state: BlockState, pos: BlockPos, entity: LivingEntity): Boolean
     fun onEntityHit(stack: ItemStack, target: LivingEntity, attacker: LivingEntity): Boolean
-
+    fun tick() {
+        cooldown--
+    }
+    fun canHave(other: EnergyAbility): Boolean {
+        return other::class != this::class
+    }
+    fun canCombine(other: EnergyAbility): Boolean {
+        return other.canHave(this) && this.canHave(other)
+    }
 }
--- src/main/kotlin/olivermakesco/de/copper/item/EnergizableItem.kt
+++ src/main/kotlin/olivermakesco/de/copper/item/EnergizableItem.kt
@@ -1,9 +1,28 @@
 package olivermakesco.de.copper.item
 
 import net.minecraft.item.ItemStack
+import olivermakesco.de.copper.energy.EnergyAbility
 
 interface EnergizableItem {
-    fun getEnergyTimers(stack:ItemStack): Array<Int>
-
+    fun getEnergyAbilities(stack: ItemStack): ArrayList<EnergyAbility>
+    fun addEnergyAbility(stack: ItemStack, ability: EnergyAbility) {
+        val energyAbilities = getEnergyAbilities(stack)
+        var combine = true
+        for (a2 in energyAbilities)
+            if (!ability.canCombine(a2)) {
+                combine = false
+                break
+            }
+        if (!combine) return
+        energyAbilities.add(ability)
+        setEnergyAbilities(stack,energyAbilities)
+    }
+    fun setEnergyAbilities(stack: ItemStack, abilities: ArrayList<EnergyAbility>)
+    fun tickAll(stack: ItemStack) {
+        val energyAbilities = getEnergyAbilities(stack)
+        for (ability in energyAbilities)
+            ability.tick()
+        setEnergyAbilities(stack, energyAbilities)
+    }
 
 }
